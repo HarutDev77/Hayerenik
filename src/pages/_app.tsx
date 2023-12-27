02e17e2906ea21a014ca4c58c1c1ba3f17de9129
Agh-Hayk <agh.hayk97@mail.ru>
2023-12-27 18:24:30 +0400

    react query config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,33 @@
-import type {AppProps} from 'next/app'
-import {NextPage} from "next";
-import {ReactElement, ReactNode} from "react";
-import '@/assets/styles/index.scss';
-import 'react-toastify/dist/ReactToastify.css'
-
-import {defaultOptions} from "@/configs/reaqtQueryConfigs";
-import {ToastContainer} from "react-toastify";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import "@/assets/styles/index.scss";
+import "react-toastify/dist/ReactToastify.css";
+import { defaultOptions } from "@/configs/reaqtQueryConfigs";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export type NextPageWithLayout = NextPage & {
-    getLayout?: (page: ReactElement) => ReactNode
-}
+   getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout
-}
+   Component: NextPageWithLayout;
+};
 
 const queryClient = new QueryClient({
-    defaultOptions: defaultOptions,
-})
-export default function App({Component, pageProps}: AppPropsWithLayout) {
-    const getLayout = Component.getLayout ?? ((page) => page)
-    return (
-        <QueryClientProvider client={queryClient}>
-        <ToastContainer position='bottom-right' />
-             {getLayout(<Component {...pageProps} />)}
-        </QueryClientProvider>
-    )
+   defaultOptions: defaultOptions,
+});
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+   const getLayout = Component.getLayout ?? ((page) => page);
+   return (
+      <QueryClientProvider client={queryClient}>
+         <ToastContainer
+            position="top-right"
+            theme="colored"
+            pauseOnFocusLoss
+         />
+         {getLayout(<Component {...pageProps} />)}
+      </QueryClientProvider>
+   );
 }
--- src/pages/admin/login/index.tsx
+++ src/pages/admin/login/index.tsx
@@ -1,153 +1,144 @@
-import React, {FC, useState} from 'react';
-import {Button, Checkbox, Form, Input, Modal, Typography} from "antd";
-import {LockOutlined, MailOutlined} from "@ant-design/icons";
+import React, { FC, useState } from "react";
+import { Button, Checkbox, Form, Input, Modal, Typography } from "antd";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
 import classes from "./Login.module.scss";
-import {useRouter} from "next/router";
-import axiosRequest from "@/api/axiosRequest";
-import {setAuthToken} from "@/api/auth";
-import {getCookie} from "cookies-next";
-import {useMutation} from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { setAuthToken } from "@/api/auth";
+import { getCookie } from "cookies-next";
+import { useMutation } from "react-query";
+import AuthApi from "@/api/auth.api";
+import { ILoginData } from "@/types/admin";
+
 const { Text, Title, Link } = Typography;
 
 export async function getServerSideProps(context: any) {
-    const { req, res } = context;
+   const { req, res } = context;
 
-    const token = getCookie('token', {
-        req,
-        res,
-        httpOnly: false,
-        secure: true,
-        sameSite: 'none'
-    });
+   const token = getCookie("token", {
+      req,
+      res,
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+   });
 
-    if(token){
-       return {
-            redirect: {
-                destination: "/admin",
-                permanent: false,
-            }
-        }
-    }
+   if (token) {
+      return {
+         redirect: {
+            destination: "/admin",
+            permanent: false,
+         },
+      };
+   }
 
-    return {
-        props: {}
-    };
+   return {
+      props: {},
+   };
 }
 
-const Login:FC = () => {
+const Login: FC = () => {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const router = useRouter();
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [message,setMessage] = useState("")
-    const router = useRouter();
+   const { mutate } = useMutation(
+      (data: ILoginData) => {
+         return AuthApi.login(data);
+      },
+      {
+         onSuccess: (data) => {
+            setAuthToken(data.data.resData);
+            router.push("/admin");
+         },
+      }
+   );
 
-    const handleOk = () => {
-        setIsModalOpen(false);
-    };
-
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-
-    const loginHandler = async () => {
-        try {
-            const response = await axiosRequest.post("admin/login", {
-                "email": email,
-                "password": password
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            setAuthToken(response.data.resData);
-
-             await router.push('/admin');
-        } catch (error) {
-            setMessage(error.message)
-            setIsModalOpen(prevState => !prevState)
-        }
-    }
-
-
-
-    return (
-        <section className={classes.hk_login_container}>
-            <div>
-                <div className={classes.hk_login_container_header}>
-                    <Title className={classes.hk_login_container_title}>Sign in</Title>
-                    <Text className={classes.hk_login_container_text}>
-                        Welcome back to Hayerenik! Please enter your details below to
-                        sign in.
-                    </Text>
-                </div>
-                <Form
-                    name="normal_login"
-                    initialValues={{
-                        remember: true,
-                    }}
-                    layout="vertical"
-                    requiredMark="optional"
-                >
-                    <Form.Item
-                        name="email"
-                        rules={[
-                            {
-                                type: "email",
-                                required: true,
-                                message: "Please input your Email!",
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={<MailOutlined />}
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e)=>setEmail(e.target.value)}
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your Password!",
-                            },
-                        ]}
-                    >
-                        <Input.Password
-                            prefix={<LockOutlined />}
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e)=> setPassword(e.target.value)}
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-                        <a className={classes.hk_login_container_forgot_password} href="">
-                            Forgot password?
-                        </a>
-                    </Form.Item>
-                    <Form.Item style={{ marginBottom: "0px" }}>
-                        <Button block="true" type="primary" htmlType="submit" onClick={loginHandler}>
-                            Log in
-                        </Button>
-                        <div className={classes.hk_login_footer}>
-                            <Text className={classes.hk_login_container_text} >{"Don't have an account? "}</Text>{" "}
-                            <Link href="">Sign up now</Link>
-                        </div>
-                    </Form.Item>
-                </Form>
+   return (
+      <section className={classes.hk_login_container}>
+         <div>
+            <div className={classes.hk_login_container_header}>
+               <Title className={classes.hk_login_container_title}>
+                  Sign in
+               </Title>
+               <Text className={classes.hk_login_container_text}>
+                  Welcome back to Hayerenik! Please enter your details below to
+                  sign in.
+               </Text>
             </div>
-            <Modal title="Error" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <p>{message}</p>
-            </Modal>
-        </section>
-    );
-}
+            <Form
+               name="normal_login"
+               initialValues={{
+                  remember: true,
+               }}
+               layout="vertical"
+               requiredMark="optional"
+            >
+               <Form.Item
+                  name="email"
+                  rules={[
+                     {
+                        type: "email",
+                        required: true,
+                        message: "Please input your Email!",
+                     },
+                  ]}
+               >
+                  <Input
+                     prefix={<MailOutlined />}
+                     placeholder="Email"
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                  />
+               </Form.Item>
+               <Form.Item
+                  name="password"
+                  rules={[
+                     {
+                        required: true,
+                        message: "Please input your Password!",
+                     },
+                  ]}
+               >
+                  <Input.Password
+                     prefix={<LockOutlined />}
+                     type="password"
+                     placeholder="Password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                  />
+               </Form.Item>
+               <Form.Item>
+                  <Form.Item name="remember" valuePropName="checked" noStyle>
+                     <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
+                  <a
+                     className={classes.hk_login_container_forgot_password}
+                     href=""
+                  >
+                     Forgot password?
+                  </a>
+               </Form.Item>
+               <Form.Item style={{ marginBottom: "0px" }}>
+                  <Button
+                     type="primary"
+                     htmlType="submit"
+                     onClick={() => {
+                        mutate({ email, password });
+                     }}
+                  >
+                     Log in
+                  </Button>
+                  <div className={classes.hk_login_footer}>
+                     <Text className={classes.hk_login_container_text}>
+                        {"Don't have an account? "}
+                     </Text>{" "}
+                     <Link href="">Sign up now</Link>
+                  </div>
+               </Form.Item>
+            </Form>
+         </div>
+      </section>
+   );
+};
 
 export default Login;
